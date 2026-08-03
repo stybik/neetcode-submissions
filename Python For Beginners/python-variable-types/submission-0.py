418153098adc395f255c2053082b6b8f4ae46be2
Bikram Biswas <bikrocksbiswas@gmail.com>
2026-08-03 23:20:16 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 1
+float_type = 1.0
+bool_type = True
+string_type = "Bikram"
+list_type = [1,2]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
