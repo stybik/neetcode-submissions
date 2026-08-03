9ddaaa2bbc41cbfe04934c598354c94ab2379a01
Bikram Biswas <bikrocksbiswas@gmail.com>
2026-08-03 23:21:30 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable=1
+print(type(variable))
+variable=1.0
+print(type(variable))
+variable=True
+print(type(variable))
+variable="Hello"
+print(type(variable))
+variable=[1,2]
+print(type(variable))
